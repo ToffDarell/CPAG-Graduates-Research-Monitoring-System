85f4a286b3ee5d4002aa4d2243baf677f0229ba6
Toff Darell Vergara <2301110636@student.buksu.edu.ph>
2025-12-05 12:45:38 +0800

    I updated gitignore and server configuration for environment setup

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ import cors from 'cors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.join(__dirname, '..', '.env') });
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const PORT = process.env.PORT || 5000;
 
